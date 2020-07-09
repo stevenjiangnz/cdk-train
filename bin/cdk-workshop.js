0690076d9a0a2e7e56609da7f9f0253c31a58e91
Steven Jiang <stevenjaingnz@gmail.com>
2020-07-09 11:40:59 +1000

    add js files

---
+++ bin/cdk-workshop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const cdk = require("@aws-cdk/core");
+const cdk_workshop_stack_1 = require("../lib/cdk-workshop-stack");
+const app = new cdk.App();
+new cdk_workshop_stack_1.CdkWorkshopStack(app, 'CdkWorkshopStack');
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY2RrLXdvcmtzaG9wLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiY2RrLXdvcmtzaG9wLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztBQUNBLHFDQUFxQztBQUNyQyxrRUFBNkQ7QUFFN0QsTUFBTSxHQUFHLEdBQUcsSUFBSSxHQUFHLENBQUMsR0FBRyxFQUFFLENBQUM7QUFDMUIsSUFBSSxxQ0FBZ0IsQ0FBQyxHQUFHLEVBQUUsa0JBQWtCLENBQUMsQ0FBQyIsInNvdXJjZXNDb250ZW50IjpbIiMhL3Vzci9iaW4vZW52IG5vZGVcbmltcG9ydCAqIGFzIGNkayBmcm9tICdAYXdzLWNkay9jb3JlJztcbmltcG9ydCB7IENka1dvcmtzaG9wU3RhY2sgfSBmcm9tICcuLi9saWIvY2RrLXdvcmtzaG9wLXN0YWNrJztcblxuY29uc3QgYXBwID0gbmV3IGNkay5BcHAoKTtcbm5ldyBDZGtXb3Jrc2hvcFN0YWNrKGFwcCwgJ0Nka1dvcmtzaG9wU3RhY2snKTtcbiJdfQ==
